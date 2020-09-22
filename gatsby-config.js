a20f04689fe62b0b1e0e4dc5e5bcd6388fd10d35
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-09-22 23:23:46 +0300

    Setup seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  plugins: [],
+  siteMetadata: {
+    title: `Otto Ahoniemi - fullstack developer from Helsinki`,
+    description: `Homesite for Otto Ahoniemi, fullstack developer currently studying Computer Science at Helsinki University.`,
+    author: `Otto Ahoniemi`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`
+  ],
 }
